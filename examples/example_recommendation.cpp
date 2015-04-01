586aed8c189a288634a908b7e42a3330be6460ba
Sylvus <eliass@student.ethz.ch>
2015-04-01 17:36:29 +0200

    Support for multiple users added.

--- examples/example_recommendation.cpp
+++ examples/example_recommendation.cpp
@@ -9,16 +9,16 @@ using namespace dai;
 using namespace std;
 
 
-Factor createFactorRecommendation( const Var &n1, const Var &n2, Real alpha) {
-    VarSet var_set = VarSet( n1, n2 );
+Factor createFactorRecommendation(const Var &n1, const Var &n2, Real alpha) {
+    VarSet var_set = VarSet(n1, n2);
     Factor fac(var_set);
     map<Var, size_t> state;
-    for (size_t i=0; i<n1.states(); ++i) {
+    for (size_t i = 0; i < n1.states(); ++i) {
         state[n1] = i;
-        for (size_t j=0; j<n2.states(); ++j) {
+        for (size_t j = 0; j < n2.states(); ++j) {
             state[n2] = j;
             size_t index = calcLinearState(var_set, state);
-            if (i==j) {
+            if (i == j) {
                 fac.set(index, 0.5 + alpha);
             } else {
                 fac.set(index, 0.5 - alpha);
@@ -39,24 +39,24 @@ FactorGraph example2fg() {
     vars.reserve(N + M);
 
     // Create a binary variable for each movie/person
-    for( size_t i = 0; i < N+M; i++ )
+    for (size_t i = 0; i < N + M; i++)
         vars.push_back(Var(i, 2));
 
     factors.push_back(createFactorRecommendation(vars[0], vars[N], alpha));
-    factors.push_back(createFactorRecommendation(vars[0], vars[N+1], alpha));
-    factors.push_back(createFactorRecommendation(vars[0], vars[N+2], alpha));
-    factors.push_back(createFactorRecommendation(vars[0], vars[N+3], alpha));
-    factors.push_back(createFactorRecommendation(vars[1], vars[N+1], alpha));
-    factors.push_back(createFactorRecommendation(vars[1], vars[N+2], alpha));
-    factors.push_back(createFactorRecommendation(vars[1], vars[N+3], alpha));
-    factors.push_back(createFactorRecommendation(vars[2], vars[N+2], alpha));
-    factors.push_back(createFactorRecommendation(vars[2], vars[N+3], alpha));
-    factors.push_back(createFactorRecommendation(vars[3], vars[N+1], alpha));
-    factors.push_back(createFactorRecommendation(vars[3], vars[N+2], alpha));
-    factors.push_back(createFactorRecommendation(vars[3], vars[N+3], alpha));
-    factors.push_back(createFactorRecommendation(vars[4], vars[N+1], alpha));
-    factors.push_back(createFactorRecommendation(vars[4], vars[N+2], alpha));
-    factors.push_back(createFactorRecommendation(vars[4], vars[N+3], alpha));
+    factors.push_back(createFactorRecommendation(vars[0], vars[N + 1], alpha));
+    factors.push_back(createFactorRecommendation(vars[0], vars[N + 2], alpha));
+    factors.push_back(createFactorRecommendation(vars[0], vars[N + 3], alpha));
+    factors.push_back(createFactorRecommendation(vars[1], vars[N + 1], alpha));
+    factors.push_back(createFactorRecommendation(vars[1], vars[N + 2], alpha));
+    factors.push_back(createFactorRecommendation(vars[1], vars[N + 3], alpha));
+    factors.push_back(createFactorRecommendation(vars[2], vars[N + 2], alpha));
+    factors.push_back(createFactorRecommendation(vars[2], vars[N + 3], alpha));
+    factors.push_back(createFactorRecommendation(vars[3], vars[N + 1], alpha));
+    factors.push_back(createFactorRecommendation(vars[3], vars[N + 2], alpha));
+    factors.push_back(createFactorRecommendation(vars[3], vars[N + 3], alpha));
+    factors.push_back(createFactorRecommendation(vars[4], vars[N + 1], alpha));
+    factors.push_back(createFactorRecommendation(vars[4], vars[N + 2], alpha));
+    factors.push_back(createFactorRecommendation(vars[4], vars[N + 3], alpha));
     Factor fac1(vars[2]);
     fac1.set(0, 0.9);
     Factor fac2(vars[3]);
@@ -66,22 +66,22 @@ FactorGraph example2fg() {
 
     // Create the factor graph out of the variables and factors
     cout << "Creating the factor graph..." << endl;
-    return FactorGraph( factors.begin(), factors.end(), vars.begin(), vars.end(), factors.size(), vars.size() );
+    return FactorGraph(factors.begin(), factors.end(), vars.begin(), vars.end(), factors.size(), vars.size());
 }
 
 
-double doInference( FactorGraph& fg, string algOpts, size_t maxIter, double tol, vector<double> &m) {
+double doInference(FactorGraph &fg, string algOpts, size_t maxIter, double tol, vector<double> &m) {
     // Construct inference algorithm
     cout << "Inference algorithm: " << algOpts << endl;
     cout << "Constructing inference algorithm object..." << endl;
-    InfAlg* ia = newInfAlgFromString( algOpts, fg );
+    InfAlg *ia = newInfAlgFromString(algOpts, fg);
 
     // Initialize inference algorithm
     cout << "Initializing inference algorithm..." << endl;
     ia->init();
 
     // Initialize vector for storing the recommendations
-    m = vector<double>( fg.nrVars(), 0.0 );
+    m = vector<double>(fg.nrVars(), 0.0);
 
     // maxDiff stores the current convergence level
     double maxDiff = 1.0;
@@ -89,13 +89,13 @@ double doInference( FactorGraph& fg, string algOpts, size_t maxIter, double tol,
     // Iterate while maximum number of iterations has not been
     // reached and requested convergence level has not been reached
     cout << "Starting inference algorithm..." << endl;
-    for( size_t iter = 0; iter < maxIter && maxDiff > tol; iter++ ) {
+    for (size_t iter = 0; iter < maxIter && maxDiff > tol; iter++) {
         // Set recommendations to beliefs
-        for( size_t i = 0; i < fg.nrVars(); i++ )
+        for (size_t i = 0; i < fg.nrVars(); i++)
             m[i] = ia->beliefV(i)[0];
 
         // Perform the requested inference algorithm for only one step
-        ia->setMaxIter( iter + 1 );
+        ia->setMaxIter(iter + 1);
         maxDiff = ia->run();
 
         // Output progress
@@ -118,7 +118,7 @@ vector<vector<pair<int, int> > > extract_ratings(string file_name) {
     int num_entries;
     fin >> num_entries;
 
-    for (int i=0; i<num_entries; ++i) {
+    for (int i = 0; i < num_entries; ++i) {
         int user, movie, rating;
         long long time;
         fin >> user >> movie >> rating >> time;
@@ -133,13 +133,13 @@ vector<vector<pair<int, int> > > extract_ratings(string file_name) {
 }
 
 
-FactorGraph data2fg(const vector<vector<pair<int, int> > >& votings, int user) {
+FactorGraph data2fg(const vector<vector<pair<int, int> > > &votings, int user) {
     // We will create a variable for every potential user/movie. We know the number of users, let us estimate the number of movies.
     int num_users = votings.size();
     int num_movies = 0;
-    for (size_t i=0; i<votings.size(); ++i) {
+    for (size_t i = 0; i < votings.size(); ++i) {
         if (votings[i].size() > 0) {
-                    num_movies = max(num_movies, votings[i][votings[i].size() - 1].first);
+            num_movies = max(num_movies, votings[i][votings[i].size() - 1].first);
         }
     }
     Real alpha = 0.0001;
@@ -150,11 +150,11 @@ FactorGraph data2fg(const vector<vector<pair<int, int> > >& votings, int user) {
     // Reserve memory for the variables
     vars.reserve(num_users + num_movies);
     // Create a binary variable for each movie/person
-    for( size_t i = 0; i < num_users + num_movies; i++ )
+    for (size_t i = 0; i < num_users + num_movies; i++)
         vars.push_back(Var(i, 2));
 
-    for (size_t i=0; i<votings.size(); ++i) {
-        for (size_t j=0; j<votings[i].size(); ++j) {
+    for (size_t i = 0; i < votings.size(); ++i) {
+        for (size_t j = 0; j < votings[i].size(); ++j) {
             if (votings[i][j].second >= threshold) {
                 factors.push_back(createFactorRecommendation(vars[i], vars[num_users + votings[i][j].first], alpha));
             }
@@ -165,18 +165,18 @@ FactorGraph data2fg(const vector<vector<pair<int, int> > >& votings, int user) {
     int normalization_factor_p = 4;
     double sum = 0;
     double sq_sum = 0;
-    for (size_t i=0; i<votings[user].size(); ++i) {
+    for (size_t i = 0; i < votings[user].size(); ++i) {
         sum += votings[user][i].second;
     }
     double mean = sum / votings[user].size();
-    for (size_t i=0; i<votings[user].size(); ++i) {
+    for (size_t i = 0; i < votings[user].size(); ++i) {
         sq_sum += pow(votings[user][i].second - mean, 2);
     }
     double stdev = std::sqrt(sq_sum / votings[user].size());
 
-    for (size_t i=0; i<votings[user].size(); ++i) {
+    for (size_t i = 0; i < votings[user].size(); ++i) {
         Factor fac(vars[num_users + votings[user][i].first]);
-        double like = max(0.1, min(0.9, 0.5 + (votings[user][i].second - mean)/(stdev * normalization_factor_p)));
+        double like = max(0.1, min(0.9, 0.5 + (votings[user][i].second - mean) / (stdev * normalization_factor_p)));
         //cout << votings[user][i].second << " to " << like << endl;
         fac.set(0, like);
         factors.push_back(fac);
@@ -184,19 +184,19 @@ FactorGraph data2fg(const vector<vector<pair<int, int> > >& votings, int user) {
 
     // Create the factor graph out of the variables and factors
     cout << "Creating the factor graph..." << endl;
-    return FactorGraph(factors.begin(), factors.end(), vars.begin(), vars.end(), factors.size(), vars.size() );
+    return FactorGraph(factors.begin(), factors.end(), vars.begin(), vars.end(), factors.size(), vars.size());
 }
 
-pair<double, double> getPrecisionAndRecall(const vector<vector<pair<int, int> > >& test_data,
-                           const vector<pair<double, int> >& ratings, int user, size_t N) {
+pair<double, double> getPrecisionAndRecall(const vector<vector<pair<int, int> > > &test_data,
+                                           const vector<pair<double, int> > &ratings, int user, size_t N) {
     // get the top predicted elements.
     vector<int> predicted;
-    for (size_t i=0; i < std::min(N, ratings.size()); ++i) {
+    for (size_t i = 0; i < std::min(N, ratings.size()); ++i) {
         predicted.push_back(ratings[i].second);
     }
 
     vector<int> wanted;
-    for (size_t i=0; i < test_data[user].size(); ++i) {
+    for (size_t i = 0; i < test_data[user].size(); ++i) {
         if (test_data[user][i].second >= 5) {
             wanted.push_back(test_data[user][i].first);
         }
@@ -206,21 +206,23 @@ pair<double, double> getPrecisionAndRecall(const vector<vector<pair<int, int> > 
     vector<int> v(N);
     sort(predicted.begin(), predicted.end());
     sort(wanted.begin(), wanted.end());
-    vector<int>::iterator  it = set_intersection(predicted.begin(), predicted.end(), wanted.begin(), wanted.end(), v.begin());
-    v.resize(it-v.begin());
+    vector<int>::iterator it = set_intersection(predicted.begin(), predicted.end(), wanted.begin(), wanted.end(),
+                                                v.begin());
+    v.resize(it - v.begin());
 
     //std::cout << "The intersection has " << (v.size()) << " elements:\n";
-    return make_pair<double, double> (v.size() / static_cast<double>(N), v.size() / static_cast<double>(wanted.size()));
+    return make_pair<double, double>(v.size() / static_cast<double>(N), v.size() / static_cast<double>(wanted.size()));
 }
 
 
 /// Main program
-int main(int argc,char **argv) {
-    cimg_usage( "This example shows how libDAI can be used for a simple recommendation task" );
-    const char *infname = cimg_option( "-method", "BP[updates=SEQMAX,maxiter=100,tol=1e-19,logdomain=0]", "Inference method in format name[key1=val1,...,keyn=valn]" );
-    const size_t maxiter = cimg_option( "-maxiter", 100, "Maximum number of iterations for inference method" );
-    const bool run_toy_example = cimg_option( "-run_toy_example", 0, "Should we only run a small example?" );
-    const double tol = cimg_option( "-tol", 1e-19, "Desired tolerance level for inference method" );
+int main(int argc, char **argv) {
+    cimg_usage("This example shows how libDAI can be used for a simple recommendation task");
+    const char *infname = cimg_option("-method", "BP[updates=SEQMAX,maxiter=100,tol=1e-15,logdomain=0]",
+                                      "Inference method in format name[key1=val1,...,keyn=valn]");
+    const size_t maxiter = cimg_option("-maxiter", 100, "Maximum number of iterations for inference method");
+    const bool run_toy_example = cimg_option("-run_toy_example", 0, "Should we only run a small example?");
+    const double tol = cimg_option("-tol", 1e-15, "Desired tolerance level for inference method");
 
     if (run_toy_example) {
         FactorGraph fg = example2fg();
@@ -228,34 +230,52 @@ int main(int argc,char **argv) {
         cout << "Solving the inference problem...please be patient!" << endl;
         doInference(fg, infname, maxiter, tol, m);
         cout << "Printing result: " << endl;
-        for (int i=0; i<m.size(); ++i) {
+        for (int i = 0; i < m.size(); ++i) {
             cout << i << " : " << m[i] << endl;
         }
     } else {
         cout << "reading data now..." << endl;
         vector<vector<pair<int, int> > > input_data = extract_ratings("u1.base");
         vector<vector<pair<int, int> > > test_data = extract_ratings("u1.test");
-        int user = 5;
-        cout << "building factor graph..." << endl;
-        FactorGraph fg = data2fg(input_data, user);
+        const int N = 10;
+        double p10 = 0;
+        double p20 = 0;
+        double r10 = 0;
+        double r20 = 0;
+        for (int user=0; user<N; ++user) {
+            cout << "building factor graph for user " << user << endl;
+            FactorGraph fg = data2fg(input_data, user);
 
-        vector<double> m; // Stores the final recommendations
-        cout << "Solving the inference problem...please be patient!" << endl;
-        doInference(fg, infname, maxiter, tol, m);
-        cout << "Printing result: " << endl;
-        vector<pair<double, int> > ratings;
-        for (size_t i = input_data.size(); i<m.size(); ++i) {
-            // push back the negative so we can use the standard sorting.
-            ratings.push_back(make_pair<double, int>(-m[i], i - input_data.size() + 1));
+            vector<double> m; // Stores the final recommendations
+            cout << "Solving the inference problem...please be patient!" << endl;
+            doInference(fg, infname, maxiter, tol, m);
+            vector<pair<double, int> > ratings;
+            for (size_t i = input_data.size(); i < m.size(); ++i) {
+                // push back the negative so we can use the standard sorting.
+                ratings.push_back(make_pair<double, int>(-m[i], i - input_data.size() + 1));
+            }
+            sort(ratings.begin(), ratings.end());
+            pair<double, double> pr10 = getPrecisionAndRecall(test_data, ratings, user, 10);
+            pair<double, double> pr20 = getPrecisionAndRecall(test_data, ratings, user, 20);
+
+            p10 +=  pr10.first;
+            p20 +=  pr20.first;
+            r10 +=  pr10.second;
+            r20 +=  pr20.second;
+            cout << "Precision (N=10): " << pr10.first << endl;
+            cout << "Precision (N=20): " << pr20.first << endl;
+            cout << "Recall (N=10): " << pr10.second << endl;
+            cout << "Recall (N=10): " << pr20.second << endl;
         }
-        sort(ratings.begin(), ratings.end());
-        pair<double, double> pr10 = getPrecisionAndRecall(test_data, ratings, user, 10);
-        pair<double, double> pr20 = getPrecisionAndRecall(test_data, ratings, user, 20);
-
-        cout <<"Precision (N=10): " << pr10.first << endl;
-        cout <<"Precision (N=20): " << pr20.first << endl;
-        cout <<"Recall (N=10): " << pr10.second << endl;
-        cout <<"Recall (N=10): " << pr20.second << endl;
+        p10 = p10 / static_cast<double>(N);
+        p20 = p20 / static_cast<double>(N);
+        r10 = r20 / static_cast<double>(N);
+        r20 = r20 / static_cast<double>(N);
+        cout << "Final estimated:" << endl;
+        cout << "Precision (N=10): " << p10 << endl;
+        cout << "Precision (N=20): " << p20 << endl;
+        cout << "Recall (N=10): " << r10 << endl;
+        cout << "Recall (N=10): " << r20 << endl;
     }
     return 0;
 }
