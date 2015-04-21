c4eff7af5908f530c0c4c35571f9fd0148ed0892
Flurin Rindisbacher <info@flurischt.ch>
2015-04-21 16:28:04 +0200

    add simple testscript

---
+++ build_and_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# jenkins uses this script to build and test
+BUILD_DIR=./testBuild/
+
+rm -rf $BUILD_DIR
+mkdir $BUILD_DIR
+cd $BUILD_DIR
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
+cd examples/
+./example_recommendation -dataset u1 -test
+# TODO: archive measured cycles, time and git-commit-id for each build
