93ee76297368a21694ed1c8f4433d34e1ce304d2
Flurin Rindisbacher <info@flurischt.ch>
2015-04-21 23:11:40 +0200

    set cpufreq of server for testing

--- build_and_test.sh
+++ build_and_test.sh
@@ -8,5 +8,5 @@ cd $BUILD_DIR
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make
 cd examples/
-./example_recommendation -dataset u1 -test
+./example_recommendation -dataset u1 -test -cpufreq 2300000000
 # TODO: archive measured cycles, time and git-commit-id for each build
