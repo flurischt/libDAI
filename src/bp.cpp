78d9540f360e64e68a5dc1cb8913523960134503
normanius <normanius@gmail.com>
2015-05-17 14:55:08 +0200

    Avoid indirect lookup (via _indices) if possible. This brings a bit of speedup for the Recommender, but only very little (<3%)

--- src/bp.cpp
+++ src/bp.cpp
@@ -251,23 +251,44 @@ void BP::calcNewMessage( size_t i, size_t _I) {
         if (_marg.size() != marg.size())
             _marg.resize(marg.size());
         std::fill(_marg._p.begin(), _marg._p.end(), 0.0);
-        // ind is the precalculated IndexFor(i,I) i.e. to x_I == k corresponds x_i == ind[k]
-        const ind_t& ind = index(i,_I);
-//        if (ind != ind_t({0,0,1,1}))
-//            cout << ind << " ";
-        for( size_t r = 0; r < _prod.size(); ++r )
-            _marg._p[ind[r]] = _marg[ind[r]] + _prod[r];
-        _marg.normalizeFast();
-        std::copy(_marg._p.begin(), _marg._p.end(), marg._p.begin());
+
+        // Shortcut, to avoid code duplication. We are making use of the
+        // fact that m is always of type ProbD == ProbProduct!
+        // m <-- _marg = ProbProduct(newMessage(i, _I))
+        ProbProduct &m = _marg;
 #else
         Prob &marg = newMessage(i,_I);
         std::fill(marg._p.begin(), marg._p.end(), 0.0);
 
-        // ind is the precalculated IndexFor(i,I) i.e. to x_I == k corresponds x_i == ind[k]
-        const ind_t& ind = index(i,_I);
-        for( size_t r = 0; r < _prod.size(); ++r )
-            marg._p[ind[r]] = marg[ind[r]] + _prod[r];
-        marg.normalizeFast();
+        // Shortcut, to avoid code duplication. We are making use of the
+        // fact that m is always of type ProbD == ProbProduct!
+        // m <-- marg = newMessage(i, _I)
+        ProbProduct &m = marg;
+#endif
+
+        // Avoid the indirect lookup via ind_t if possible.
+        switch (_edges[i][_I].index) {
+            case INDEX_0011:
+                m._p[0] += (_prod._p[0]+_prod._p[1]);
+                m._p[1] += (_prod._p[2]+_prod._p[3]);
+                break;
+            case INDEX_0101:
+                m._p[0] += (_prod._p[0]+_prod._p[2]);
+                m._p[1] += (_prod._p[1]+_prod._p[3]);
+                break;
+            default: {
+                // ind is the precalculated IndexFor(i,I) i.e. to x_I == k
+                // corresponds x_i == ind[k]
+                const ind_t& ind = index(i,_I);
+                for( size_t r = 0; r < _prod.size(); ++r )
+                    m._p[ind[r]] += _prod[r];
+            }
+        }
+        m.normalizeFast();
+
+#ifdef DAI_SINGLE_PRECISION
+        // Copy (and cast) from ProbProduct to Prob.
+        std::copy(m._p.begin(), m._p.end(), marg._p.begin());
 #endif
     }
 
