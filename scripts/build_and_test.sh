ea250ae0e540ec9a438d86d22e6d936c33ca3845
Flurin Rindisbacher <info@flurischt.ch>
2015-05-25 16:27:51 +0200

    measure (and publish) double and float

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 # jenkins uses this script to build and test
 BUILD_DIR=./testBuild/
+FLOAT_BUILD_DIR=./testBuildFloat/
 
+# compile double
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
 cd $BUILD_DIR
 cmake -DCMAKE_BUILD_TYPE=Release -DWITH_PERF=TRUE -DWITH_RECOMMENDER_BOOST=TRUE ..
 make -j8
 cd examples/
-./example_recommendation -dataset u1 -numMeasurements 7 -test -cpufreq 2300000000 > output.txt
-cat output.txt
+cp ../../examples/new_datasets/ml100kfull.* .
+cp ../../examples/new_datasets/u1/32MoviesUID1.* .
+cp ../../examples/new_datasets/u1/512MoviesUID1.* .
+
+# compile float
+cd ../..
+rm -rf $FLOAT_BUILD_DIR
+mkdir $FLOAT_BUILD_DIR
+cd $FLOAT_BUILD_DIR
+cmake -DCMAKE_BUILD_TYPE=Release -DWITH_PERF=TRUE -DWITH_RECOMMENDER_BOOST=TRUE -DWITH_SINGLE_PRECISION=TRUE ..
+make -j8
+cd examples/
+cp ../../examples/new_datasets/ml100kfull.* .
+cp ../../examples/new_datasets/u1/32MoviesUID1.* .
+cp ../../examples/new_datasets/u1/512MoviesUID1.* .
+
+# test double
+cd ../../$BUILD_DIR/examples/
+./example_recommendation -dataset u1 -numMeasurements 7 -test -cpufreq 2300000000 > /dev/null
+./example_recommendation -dataset ml100kfull -numMeasurements 7 -cpufreq 2300000000 > output_ml100kfull.txt
+cat output_ml100kfull.txt
+./example_recommendation -dataset 32MoviesUID1 -numMeasurements 7 -cpufreq 2300000000 > output_32.txt
+cat output_32.txt
+./example_recommendation -dataset 512MoviesUID1 -numMeasurements 7 -cpufreq 2300000000 > output_512.txt
+cat output_512.txt
+
+# test float
+cd ../../$FLOAT_BUILD_DIR/examples/
+./example_recommendation -dataset ml100kfull -numMeasurements 7 -cpufreq 2300000000 > output_ml100kfull_float.txt
+cat output_ml100kfull_float.txt
--- scripts/publish_measurements.sh
+++ scripts/publish_measurements.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-printf "`git log -n1 | head -n5 | grep -v '^$'` \n`cat testBuild/examples/output.txt | tail -n4`\n\n" | curl --user $FTP_USER:$FTP_PW --ftp-create-dirs -a -T - ftp://server34.cyon.ch/are_we_fast_yet.txt
+printf "`git log -n1 | head -n5 | grep -v '^$'` \n`cat testBuild/examples/output_32.txt | tail -n2`\n\n" | curl --user $FTP_USER:$FTP_PW --ftp-create-dirs -a -T - ftp://server34.cyon.ch/measurements/measurement_32.txt
+printf "`git log -n1 | head -n5 | grep -v '^$'` \n`cat testBuild/examples/output_512.txt | tail -n2`\n\n" | curl --user $FTP_USER:$FTP_PW --ftp-create-dirs -a -T - ftp://server34.cyon.ch/measurements/measurement_512.txt
+printf "`git log -n1 | head -n5 | grep -v '^$'` \n`cat testBuild/examples/output_ml100kfull.txt | tail -n2`\n\n" | curl --user $FTP_USER:$FTP_PW --ftp-create-dirs -a -T - ftp://server34.cyon.ch/measurements/measurement_ml100kfull.txt
+printf "`git log -n1 | head -n5 | grep -v '^$'` \n`cat testBuildFloat/examples/output_ml100kfull_float.txt | tail -n2`\n\n" | curl --user $FTP_USER:$FTP_PW --ftp-create-dirs -a -T - ftp://server34.cyon.ch/measurements/measurement_ml100kfull_float.txt
