8901de1fa3e39c4e5e64cb25c49dfd82a933a1b7
Flurin Rindisbacher <info@flurischt.ch>
2015-06-12 18:32:28 +0200

    remove todos and formatting in bp.h

--- include/dai/bp.h
+++ include/dai/bp.h
@@ -8,7 +8,6 @@
 
 /// \file
 /// \brief Defines class BP, which implements (Loopy) Belief Propagation
-/// \todo Consider using a priority_queue for maximum residual schedule
 
 
 #ifndef __defined_libdai_bp_h
@@ -16,6 +15,7 @@
 
 
 #include <dai/dai_config.h>
+
 #ifdef DAI_WITH_BP
 
 
@@ -80,13 +80,13 @@ namespace dai {
         typedef Real MessageType;
         struct EdgeProp {
             /// Index cached for this edge
-            size_t      index;
+            size_t index;
             /// Old message living on this edge
             MessageType message;
             /// New message living on this edge
             MessageType newMessage;
             /// Residual for this edge
-            Real        residual;
+            Real residual;
 
 #ifdef DAI_VECTORIZATION
             /// Precalculated reciprocals of the message for this edge:
@@ -112,13 +112,12 @@ namespace dai {
         // updated we also update the corresponding product. We can then reuse
         // the result and make the algorithm much faster.
         // Use double precision!
-        // TODO: use std::vector<ProbProd> (for consistent notation)
 #ifdef DAI_VECTORIZATION
         __m256d _prod;
         std::vector< __m256d,  AlignmentAllocator<__m256d, 32> > _oldProd;
         #else
         double _prod[4];
-        std::vector< std::vector<double> > _oldProd;
+        std::vector<std::vector<double> > _oldProd;
 #endif
 
 #ifdef DAI_SINGLE_PRECISION
@@ -156,14 +155,14 @@ namespace dai {
              *  - SEQMAX maximum-residual updates [\ref EMK06]
              * \obsolete BPFast uses always SEQMAX.
              */
-            DAI_ENUM(UpdateType,SEQFIX,SEQRND,SEQMAX,PARALL);
+            DAI_ENUM(UpdateType, SEQFIX, SEQRND, SEQMAX, PARALL);
 
             /// Enumeration of inference variants
             /** There are two inference variants:
              *  - SUMPROD Sum-Product
              *  - MAXPROD Max-Product (equivalent to Min-Sum)
              */
-            DAI_ENUM(InfType,SUMPROD,MAXPROD);
+            DAI_ENUM(InfType, SUMPROD, MAXPROD);
 
             /// Verbosity (amount of output sent to stderr)
             size_t verbose;
@@ -192,61 +191,33 @@ namespace dai {
         /// \name Constructors/destructors
         //@{
         /// Default constructor
-        BP() : DAIAlgFG()
-                , _edges()
-                , _indices()
-                , _oldProd()
-                , _lut()
-                , _maxdiff(0.0)
-                , _iters(0U)
-                , _sentMessages()
-                , _oldBeliefsV()
-                , _oldBeliefsF()
-                , _updateSeq()
-                , props()
-                , recordSentMessages(false)
-        {}
+        BP()
+                : DAIAlgFG(), _edges(), _indices(), _oldProd(), _lut(), _maxdiff(0.0), _iters(0U), _sentMessages(),
+                  _oldBeliefsV(), _oldBeliefsF(), _updateSeq(), props(), recordSentMessages(false) { }
 
         /// Construct from FactorGraph \a fg and PropertySet \a opts
         /** \param fg Factor graph.
          *  \param opts Parameters @see Properties
          */
-        BP( const FactorGraph & fg, const PropertySet &opts ) : DAIAlgFG(fg)
-                , _edges()
-                , _indices()
-                , _oldProd()
-                , _maxdiff(0.0)
-                , _iters(0U)
-                , _sentMessages()
-                , _oldBeliefsV()
-                , _oldBeliefsF()
-                , _updateSeq()
-                , props()
-                , recordSentMessages(false) {
-            setProperties( opts );
+        BP(const FactorGraph &fg, const PropertySet &opts) : DAIAlgFG(fg), _edges(), _indices(), _oldProd(),
+                                                             _maxdiff(0.0), _iters(0U), _sentMessages(), _oldBeliefsV(),
+                                                             _oldBeliefsF(), _updateSeq(), props(),
+                                                             recordSentMessages(false) {
+            setProperties(opts);
             construct();
         }
 
         /// Copy constructor
-        BP( const BP &x ) : DAIAlgFG(x)
-                , _edges(x._edges)
-                , _indices(x._indices)
-                , _oldProd(x._oldProd)
-                , _lut(x._lut)
-                , _maxdiff(x._maxdiff)
-                , _iters(x._iters)
-                , _sentMessages(x._sentMessages)
-                , _oldBeliefsV(x._oldBeliefsV)
-                , _oldBeliefsF(x._oldBeliefsF)
-                , _updateSeq(x._updateSeq)
-                , props(x.props)
-                , recordSentMessages(x.recordSentMessages) {
+        BP(const BP &x) : DAIAlgFG(x), _edges(x._edges), _indices(x._indices), _oldProd(x._oldProd), _lut(x._lut),
+                          _maxdiff(x._maxdiff), _iters(x._iters), _sentMessages(x._sentMessages),
+                          _oldBeliefsV(x._oldBeliefsV), _oldBeliefsF(x._oldBeliefsF), _updateSeq(x._updateSeq),
+                          props(x.props), recordSentMessages(x.recordSentMessages) {
         }
 
         /// Assignment operator
-        BP& operator=( const BP &x ) {
-            if( this != &x ) {
-                DAIAlgFG::operator=( x );
+        BP &operator=(const BP &x) {
+            if (this != &x) {
+                DAIAlgFG::operator=(x);
                 _edges = x._edges;
                 _indices = x._indices;
                 _oldProd = x._oldProd;
@@ -266,33 +237,51 @@ namespace dai {
 
         /// \name General InfAlg interface
         //@{
-        virtual BP* clone() const { return new BP(*this); }
-        virtual BP* construct( const FactorGraph &fg, const PropertySet &opts ) const { return new BP( fg, opts ); }
+        virtual BP *clone() const { return new BP(*this); }
+
+        virtual BP *construct(const FactorGraph &fg, const PropertySet &opts) const { return new BP(fg, opts); }
+
         virtual std::string name() const { return "BP"; }
-        virtual Factor belief( const Var &v ) const { return beliefV( findVar( v ) ); }
-        virtual Factor belief( const VarSet &vs ) const;
-        virtual Factor beliefV( size_t i ) const;
-        virtual Factor beliefF( size_t I ) const;
+
+        virtual Factor belief(const Var &v) const { return beliefV(findVar(v)); }
+
+        virtual Factor belief(const VarSet &vs) const;
+
+        virtual Factor beliefV(size_t i) const;
+
+        virtual Factor beliefF(size_t I) const;
+
         virtual std::vector<Factor> beliefs() const;
+
         virtual Real logZ() const;
+
         /** \pre Assumes that run() has been called and that \a props.inference == \c MAXPROD
          */
-        std::vector<size_t> findMaximum() const { return dai::findMaximum( *this ); }
+        std::vector<size_t> findMaximum() const { return dai::findMaximum(*this); }
+
         virtual void init();
-        virtual void init( const VarSet &ns );
+
+        virtual void init(const VarSet &ns);
+
         virtual Real run();
+
         virtual Real maxDiff() const { return _maxdiff; }
+
         virtual size_t Iterations() const { return _iters; }
-        virtual void setMaxIter( size_t maxiter ) { props.maxiter = maxiter; }
-        virtual void setProperties( const PropertySet &opts );
+
+        virtual void setMaxIter(size_t maxiter) { props.maxiter = maxiter; }
+
+        virtual void setProperties(const PropertySet &opts);
+
         virtual PropertySet getProperties() const;
+
         virtual std::string printProperties() const;
         //@}
 
         /// \name Additional interface specific for BP
         //@{
         /// Returns history of which messages have been updated
-        const std::vector<std::pair<size_t, size_t> >& getSentMessages() const {
+        const std::vector<std::pair<size_t, size_t> > &getSentMessages() const {
             return _sentMessages;
         }
 
@@ -302,21 +291,28 @@ namespace dai {
 
     protected:
         /// Returns constant reference to message from the \a _I 'th neighbor of variable \a i to variable \a i
-        const MessageType & message(size_t i, size_t _I) const { return _edges[i][_I].message; }
+        const MessageType &message(size_t i, size_t _I) const { return _edges[i][_I].message; }
+
         /// Returns reference to message from the \a _I 'th neighbor of variable \a i to variable \a i
-        MessageType & message(size_t i, size_t _I) { return _edges[i][_I].message; }
+        MessageType &message(size_t i, size_t _I) { return _edges[i][_I].message; }
+
         /// Returns constant reference to updated message from the \a _I 'th neighbor of variable \a i to variable \a i
-        const MessageType & newMessage(size_t i, size_t _I) const { return _edges[i][_I].newMessage; }
+        const MessageType &newMessage(size_t i, size_t _I) const { return _edges[i][_I].newMessage; }
+
         /// Returns reference to updated message from the \a _I 'th neighbor of variable \a i to variable \a i
-        MessageType & newMessage(size_t i, size_t _I) { return _edges[i][_I].newMessage; }
+        MessageType &newMessage(size_t i, size_t _I) { return _edges[i][_I].newMessage; }
+
         /// Returns constant reference to cached index for the edge between variable \a i and its \a _I 'th neighbor
-        const ind_t & index(size_t i, size_t _I) const { return _indices[_edges[i][_I].index]; }
+        const ind_t &index(size_t i, size_t _I) const { return _indices[_edges[i][_I].index]; }
+
         /// Returns reference to cached index for the edge between variable \a i and its \a _I 'th neighbor
-        ind_t & index(size_t i, size_t _I) { return _indices[_edges[i][_I].index]; }
+        ind_t &index(size_t i, size_t _I) { return _indices[_edges[i][_I].index]; }
+
         /// Returns constant reference to residual for the edge between variable \a i and its \a _I 'th neighbor
-        const Real & residual(size_t i, size_t _I) const { return _edges[i][_I].residual; }
+        const Real &residual(size_t i, size_t _I) const { return _edges[i][_I].residual; }
+
         /// Returns reference to residual for the edge between variable \a i and its \a _I 'th neighbor
-        Real & residual(size_t i, size_t _I) { return _edges[i][_I].residual; }
+        Real &residual(size_t i, size_t _I) { return _edges[i][_I].residual; }
 
         /// Calculate the product of factor \a I and the incoming messages
         /** If \a without_i == \c true, the message coming from variable \a i is omitted from the product
@@ -330,29 +326,35 @@ namespace dai {
         void marginalizeProductOntoMessage(__m256d& prod_vec, size_t i, size_t _I, size_t prodsize);
 #else
 
-        void calcIncomingMessageProduct_0101_0011(double* prod, size_t I, size_t i) const;
-        void marginalizeProductOntoMessage(double* prod, size_t i, size_t _I, size_t prodsize);
+        void calcIncomingMessageProduct_0101_0011(double *prod, size_t I, size_t i) const;
+
+        void marginalizeProductOntoMessage(double *prod, size_t i, size_t _I, size_t prodsize);
+
 #endif
 
-        static const int INDEX_0011      = 0;
-        static const int INDEX_0101      = 1;
-        static const int INDEX_01        = 2;
+        static const int INDEX_0011 = 0;
+        static const int INDEX_0101 = 1;
+        static const int INDEX_01 = 2;
         static const int INDEX_0101_0011 = 3;
 
 
         /// Calculate the updated message from the \a _I 'th neighbor of variable \a i to variable \a i
-        virtual void calcNewMessage( size_t i, size_t _I);
+        virtual void calcNewMessage(size_t i, size_t _I);
+
         /// Replace the "old" message from the \a _I 'th neighbor of variable \a i to variable \a i by the "new" (updated) message
-        void updateMessage( size_t i, size_t _I );
+        void updateMessage(size_t i, size_t _I);
 
         /// Set the residual (difference between new and old message) for the edge between variable \a i and its \a _I 'th neighbor to \a r
-        void updateResidual( size_t i, size_t _I, Real r );
+        void updateResidual(size_t i, size_t _I, Real r);
+
         /// Finds the edge which has the maximum residual (difference between new and old message)
-        bool findMaxResidual( size_t &i, size_t &_I );
+        bool findMaxResidual(size_t &i, size_t &_I);
+
         /// Calculates unnormalized belief of variable \a i
-        virtual void calcBeliefV(size_t i, ProbProduct &p ) const;
+        virtual void calcBeliefV(size_t i, ProbProduct &p) const;
+
         /// Calculates unnormalized belief of factor \a I
-        virtual void calcBeliefF( size_t I, ProbProduct &p ) const {
+        virtual void calcBeliefF(size_t I, ProbProduct &p) const {
             calcIncomingMessageProduct(p, I, false, 0);
         }
 
